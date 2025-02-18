dd4aaf57438a9e0afa7519cad4c58e153457ac14
ana <serielain@gmail.com>
2025-02-17 21:34:15 -0300

    TresProyectos

---
+++ TempleteLiteral.js
@@ -0,0 +1,10 @@
+const nombre = 'Leonardo';
+const paisNacimiento = 'Colombia';
+const paisResidencia = 'Chile';
+const deporteFvorito = 'Fútbol';
+
+const textoTemplate= `Mi nombre es ${nombre}, naci en ${paisNacimiento}.
+Actualmente vivo en ${paisResidencia}.
+Mi deporte favoito es ${deporteFvorito}.;`
+
+console.log(textoTemplate);
